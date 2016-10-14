f6611ec9453470092f7bf1c003b0d9661c378898
user <user@FH06>
2016-10-14 10:01:18 +0800

    第三周作業

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -5,13 +5,20 @@ public class StringComparator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner str = new Scanner(System.in);
-		
+		int a=1;
+		while(a==1){
 		System.out.println("Enter a string 1 :");
 		String str1 =  str.next();
 		System.out.println("Enter a string 2 :");
 		String str2 =  str.next();
-		if(str1.compareToIgnoreCase(str2)==0) System.out.println("The two strings are the same");
+		if(str1.compareToIgnoreCase(str2)==0) 
+		{
+		System.out.println("The two strings are the same");
+		System.out.println("µ{¦¡µ²§ô");
+		break;
+		}
 		else System.out.println("The two strings are not the same");
+		}
 	}
 
 }

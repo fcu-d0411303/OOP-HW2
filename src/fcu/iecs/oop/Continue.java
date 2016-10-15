4cb724a47a5e4e3c05be8831740893f673f445f8
User <User@DESKTOP-M9BMA8N>
2016-10-15 12:19:17 +0900

    修改continue

--- src/fcu/iecs/oop/Continue.java
+++ src/fcu/iecs/oop/Continue.java
@@ -5,7 +5,7 @@ public class Continue {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0;i<5;i++){
-		if(i!=1||i!=3) {continue;}
+		if(i!=1&&i!=3) {continue;}
 		System.out.println(i);
 		}
 
